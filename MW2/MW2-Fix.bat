296c92c804b4c5af7aa79b683db8ad9e957292ac
permanently <7469056+Permanently@users.noreply.github.com>
2023-03-21 13:54:16 +0000

    Added admin priv. prompt

--- MW2/MW2-Fix.bat
+++ MW2/MW2-Fix.bat
@@ -1,1 +1,11 @@
+@echo off
+fltmc >nul 2>&1 || (
+    echo Administrator privileges are required.
+    PowerShell Start -Verb RunAs '%0' 2> nul || (
+        echo Right-click on the script and select "Run as administrator".
+        pause & exit 1
+    )
+    exit 0
+)
+
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v UBR /t REG_DWORD /d 790 /f
